629f4b01188f415f7672467713f19f6720093717
NatnaelWale <nhattyjeje@gmail.com>
2024-04-25 12:12:34 -0400

    Touched up

--- IfScripts/complex_taxes.js
+++ IfScripts/complex_taxes.js
@@ -2,16 +2,19 @@
 
 //Let's bring the variables from pay rules exercise:
 
-var payRate =10
-var hoursWorked = 45
+var payRate =10;
+var hoursWorked = 45;
 var overHours;
 
 // Let's define the new known variables
 
 var annualGrossIncome;
-var filingStatus = "joint";
+var filingStatus = "single";
 var taxWithHoldings;
+var weeklyTaxWithHolding;
 var annualNetIncome;
+var weeklyGrossIncome;
+var weeklyNetIncome;
 var taxRate;
 var message;
 
@@ -26,48 +29,75 @@ if (hoursWorked > 40)
         annualGrossIncome = 52 * (payRate * hoursWorked);
      }
 
-console.log(annualGrossIncome);
+// console.log(annualGrossIncome);
 
 
-// Let's now define the if statements for different filling status and conditions
+// Let's now define the if statements for different filling status and annual gross income conditions
 
 
-if ( filingStatus == "single" && annualGrossIncome < 12000) {
-    taxRate = 0.05;
-}
-else if (filingStatus == "single" && annualGrossIncome <= 24999.99) {
-    taxRate = 0.1;
-}
-else if (filingStatus == "single" && annualGrossIncome <= 74999.99) {
-    taxRate = 0.15;
-}
-else if (filingStatus == "single" && annualGrossIncome > 75000) {
-    taxRate = 0.2;
-}
-else if ( filingStatus == "joint" && annualGrossIncome < 12000) {
-    taxRate = 0;
-}
-else if (filingStatus == "joint" && annualGrossIncome <= 24999.99) {
-    taxRate = 0.06;
-}
-else if (filingStatus == "joint" && annualGrossIncome <= 74999.99) {
-    taxRate = 0.11;
-}
-else if (filingStatus == "joint" && annualGrossIncome > 75000){
-    taxRate = 0.2;
-}
+// Using nested if statements:
+
+
+if ( filingStatus == "single") {
+    if (annualGrossIncome < 12000)          {taxRate = 0.05}
+    else if (annualGrossIncome <= 24999.99) {taxRate = 0.10}
+    else if (annualGrossIncome <= 74999.99) {taxRate = 0.15}
+    else if (annualGrossIncome > 75000)     {taxRate = 0.20}
+};
+
+if ( filingStatus == "joint") {
+    if (annualGrossIncome < 12000)          {taxRate = 0.00}
+    else if (annualGrossIncome <= 24999.99) {taxRate = 0.06}
+    else if (annualGrossIncome <= 74999.99) {taxRate = 0.11}
+    else if (annualGrossIncome > 75000)     {taxRate = 0.20}
+};
+
+
+// Using long lines of if statements:
+
+
+// if ( filingStatus == "single" && annualGrossIncome < 12000) {
+//     taxRate = 0.05;
+// }
+// else if (filingStatus == "single" && annualGrossIncome <= 24999.99) {
+//     taxRate = 0.1;
+// }
+// else if (filingStatus == "single" && annualGrossIncome <= 74999.99) {
+//     taxRate = 0.15;
+// }
+// else if (filingStatus == "single" && annualGrossIncome > 75000) {
+//     taxRate = 0.2;
+// }
+// else if ( filingStatus == "joint" && annualGrossIncome < 12000) {
+//     taxRate = 0;
+// }
+// else if (filingStatus == "joint" && annualGrossIncome <= 24999.99) {
+//     taxRate = 0.06;
+// }
+// else if (filingStatus == "joint" && annualGrossIncome <= 74999.99) {
+//     taxRate = 0.11;
+// }
+// else if (filingStatus == "joint" && annualGrossIncome > 75000){
+//     taxRate = 0.2;
+// }
 
 //  Let's now define our known formulas
 
 
 taxWithHoldings = taxRate*annualGrossIncome;
 
+weeklyTaxWithHolding = taxWithHoldings / 52;
+
 annualNetIncome = annualGrossIncome - taxWithHoldings;
+
+weeklyGrossIncome = annualGrossIncome / 52;
+
+weeklyNetIncome = annualNetIncome / 52;
 
 
 // Let's now define our result message:
 
 
-message = "You worked " + hoursWorked + "hours this period. Because you earn $" + payRate + " per hour, your gross pay is $" + annualGrossIncome + ". Your filing status is " + filingStatus + ". Your tax withholdings this period is $" + taxWithHoldings + ". Your net pay is $" + annualNetIncome + "."
+message = "You worked " + hoursWorked + " hours this period. Because you earn $" + payRate + " per hour, your gross pay is $" + weeklyGrossIncome.toFixed(2) + ". Your filing status is " + filingStatus + ". Your tax withholdings this period is $" + weeklyTaxWithHolding.toFixed(2) + ". Your net pay is $" + weeklyNetIncome.toFixed(2) + ".";
 
-console.log(message)
+console.log(message);
--- IfScripts/greetings.js
+++ IfScripts/greetings.js
@@ -2,15 +2,20 @@
 
 // Let's define our variable
 
-var time = 16;
+var time = -16000;
 
 // Let's define our if statement
 
-if (time <= 10)
-{
+if ( time < 0 ) {
+    console.log("ERROR: INVALID HOUR OF " + time);
+}
+else if (time <= 10) {
     console.log("Good Morning!");
 } else if ( time < 16 ) {
     console.log("Good Day!");
-} else {
+} else if ( time <=23 ) {
     console.log("Good Evening!");
-};
+}
+  else {
+    console.log("ERROR: INVALID HOUR OF " + time);
+}
