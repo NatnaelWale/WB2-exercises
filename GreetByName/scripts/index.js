83601c06bfdb75d4ce35d21d8ab7aedc23c19f57
NatnaelWale <nhattyjeje@gmail.com>
2024-04-26 16:00:15 -0400

    Touched up the greeting

--- GreetByName/scripts/index.js
+++ GreetByName/scripts/index.js
@@ -4,9 +4,9 @@ window.onload = init;
 
 function init() {
 
-    const onGreetUserBtnClicked = document.getElementById("greetBtn");
+    const onGreetUserBtn = document.getElementById("greetBtn");
     
-    onGreetUserBtnClicked.onclick = onClickMessage;
+    onGreetUserBtn.onclick = onClickMessage;
 
 }
 
@@ -15,7 +15,7 @@ function onClickMessage () {
     var userNameInput = document.getElementById("nameField");
 
     var greetName = userNameInput.value;
-    
+
     alert ("Hello " + greetName)
 
 }
